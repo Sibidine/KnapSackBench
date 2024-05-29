5b9f077d505aa55f1ac9feb7345545b91ed62cec
Sibidine <siddharth@HP-Pavillion.(none)>
2024-05-29 11:19:08 +0530

    feat/completed implementation of SA

--- algorithms/simulated_annealing_knapsack.py
+++ algorithms/simulated_annealing_knapsack.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 # contraints
 weights = [21, 12, 30, 24, 45, 47, 41, 36, 38, 45, 4, 17, 1, 42, 26, 19, 12, 27, 15, 4, 5, 4, 21, 7, 23, 45, 18, 7, 29, 44, 18, 3, 8, 4, 38, 23, 34, 35, 29, 32, 44, 34, 44, 24, 8, 4, 36, 16, 34, 33, 27, 36, 26, 25, 25, 47, 20, 6, 13, 35, 42, 49, 11, 39, 30, 21, 26, 25, 33, 38, 16, 5, 42, 20, 39, 9, 6, 46, 44, 50, 44, 2, 28, 50, 26, 44, 4, 50, 47, 29, 22, 17, 37, 1, 19, 47, 28, 24, 25, 16]
@@ -7,9 +8,9 @@ limit = 1500
 items = len(weights)
 
 # constants
-temperature = 12
-iterations = 1200
-step_size = 0.1
+temperature = 8000
+iterations = 120
+step_size = 50
 final_temperature = 5
 
 
@@ -37,7 +38,7 @@ def find_neighbours(member):
 
     neighbours = []
     for i in range(len(member)):
-        single_neighbour = member[:i] + list(flipped(member[i])) + member[i+1:]
+        single_neighbour = member[0:i] + [flipped(member[i])] + member[(i+1):]
         neighbours.append(single_neighbour)
     
     return neighbours
@@ -50,4 +51,63 @@ def generate_initial_permutation():
     ]
 
     return member
-    
+
+def simulated_annealing():
+
+    initial_objective = 0
+
+    while initial_objective == 0:
+        initial_config = generate_initial_permutation()
+        initial_objective = objective_function(initial_config)
+
+    best_config = initial_config
+    best_objective = initial_objective
+    coefficient = 1
+
+    metrics = {}
+    metrics["solutions_checked"] = 0
+    metrics["improvements"] = 0
+    metrics["random_steps"] = 0
+
+    while temperature/coefficient > final_temperature:
+
+        i = 0
+        improvements_current = 0
+        random_steps_current = 0
+
+        while i < step_size:
+
+            metrics["solutions_checked"] += 1
+            list_of_neighbours = find_neighbours(initial_config)
+            random_neighbour = list_of_neighbours[random.randint(0, len(list_of_neighbours)-1)]
+            random_neighbour_objective = objective_function(random_neighbour)
+            if random_neighbour_objective > best_objective:
+                best_objective = random_neighbour_objective
+                best_config = random_neighbour
+                improvements_current += 1
+            else:
+                delta = best_objective - random_neighbour_objective
+                random_value = random.uniform(0,1)
+                metropolis_criterion = math.exp(-1*delta*coefficient / temperature)
+                if(metropolis_criterion > random_value):
+                    best_objective = random_neighbour_objective
+                    best_config = random_neighbour
+                    random_steps_current += 1
+            i += 1
+        
+        metrics["improvements"] += improvements_current
+        metrics["random_steps"] += random_steps_current
+        coefficient += 1
+
+    metrics["best_config"] = best_config
+    metrics["best_objective"] = best_objective
+
+    return metrics
+
+
+
+def main():
+    metrics = simulated_annealing()
+    print(metrics["best_objective"])
+
+main()
