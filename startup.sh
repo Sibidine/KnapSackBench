d15577d76d875efb56f338f0435c8bc915138d01
Sibidine <siddharth@HP-Pavillion.(none)>
2024-05-31 15:26:02 +0530

    fix/setting ip and port tostarttup script

--- startup.sh
+++ startup.sh
@@ -5,4 +5,4 @@ pip install --upgrade -r requirements.txt
 echo "Dependencies installed."
 cd knapsackbench
 echo "Starting server..."
-uvicorn main:app --reload
+uvicorn main:app  --host 0.0.0.0 --port 8000 --reload
