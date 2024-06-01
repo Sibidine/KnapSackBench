77220e9b993b9d536e34c7e4130f7e753315fe86
Sibidine <siddharth@HP-Pavillion.(none)>
2024-06-01 14:53:42 +0530

    feat/cleaned startup script

--- startup.sh
+++ startup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "Installing dependencies"
-pip install --upgrade -r requirements.txt
-echo "Dependencies installed."
 cd knapsackbench
 echo "Starting server..."
 uvicorn main:app  --host 0.0.0.0 --port 8000 --reload
